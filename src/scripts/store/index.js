a6eaeb395f70c6fa0107fa896413f9f1f18599e9
Cameron Swaim <cpswaim@gmail.com>
2017-01-03 21:26:23 -0500

    Removing redux-logger for production builds

--- src/scripts/store/index.js
+++ src/scripts/store/index.js
@@ -3,15 +3,17 @@ import thunkMiddleware from 'redux-thunk';
 import createLogger from 'redux-logger';
 import rootReducer from '../reducers';
 
-const loggerMiddleware = createLogger();
-
+let middleware = [thunkMiddleware];
+if (process.env.NODE_ENV !== 'production') {
+    const loggerMiddleware = createLogger();
+    middleware = [...middleware, loggerMiddleware];
+}
 export default function configureStore(preloadedState) {
     return createStore(
         rootReducer,
         preloadedState,
         applyMiddleware(
-            thunkMiddleware,
-            loggerMiddleware
+            ...middleware
         )
     );
 };
